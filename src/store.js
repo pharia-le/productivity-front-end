059cc48b1ecb03ad2e0c82fb6590b8db653681f6
pharia-le <pharia.le@gmail.com>
2020-07-26 09:36:11 -0700

    Add currentUser reducer & update store.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import users from './reducers/users';
-
+import currentUser from './reducers/currentUser'
 
 const reducer = combineReducers({
-    users
+    users,
+    currentUser
   })
   
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
