ed51a4730eca90eb1fb2b8f8f967687b3bb84bcb
pharia-le <pharia.le@gmail.com>
2020-07-26 11:24:38 -0700

    Update import & store w/ loginForm.

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import users from './reducers/users';
 import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
 
 const reducer = combineReducers({
     users,
-    currentUser
+    currentUser,
+    loginForm
   })
   
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
