a0af7d3fc1b9c27179a5d54ce75dce3fc3a4ab01
pharia-le <pharia.le@gmail.com>
2020-07-26 13:35:23 -0700

    Add imports & update render() to conditionally return Login or Logout.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login'
+import Logout from './components/Logout'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 
@@ -12,11 +13,14 @@ class App extends Component {
 
   render() {
     return (
-      <>
-        <h1>Hello, I am a React App.js</h1>
-        <Login />
-      </>
+      this.props.currentUser ? <Logout /> : <Login />
     )}
 }
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
