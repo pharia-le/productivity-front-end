067b52d2a9ab1fc60c9044eafe41626462b75630
pharia-le <pharia.le@gmail.com>
2020-07-26 11:26:12 -0700

    Add Login component & content.

---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateLoginForm } from '../actions/loginForm';
+import { login } from '../actions/currentUser';
+
+const Login = ({ login, loginFormData, updateLoginForm }) => {
+    
+    const handleInputChange = event => {
+        
+        const { name, value } = event.target
+        const updatedFormInfo = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+    }
+
+    const handleOnSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <input 
+                type="text"
+                name="username"
+                placeholder="username"
+                value={loginFormData.username}
+                onChange={handleInputChange}
+            />
+            <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={loginFormData.password}
+                onChange={handleInputChange}
+            />
+            <input 
+                type="submit"
+                value="Log In"
+            />
+        </form>
+    );
+};
+
+const mapStateToProps = ({loginForm}) => {
+    return {
+        loginFormData: loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updateLoginForm, login })(Login);
