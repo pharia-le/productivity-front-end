9ba70e8fac7a930dcbe6d8f00454806656cd71c1
pharia-le <pharia.le@gmail.com>
2020-07-26 09:30:54 -0700

    Update App from functional to Class Component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello, I am a React App.js</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello, I am a React App.js</h1>
+    )}
 }
 
 export default App;
