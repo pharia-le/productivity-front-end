fbef69d4b11de11a1c0c04b00553fafaa4aa3886
pharia-le <pharia.le@gmail.com>
2020-07-28 00:09:53 -0700

    Update to return Link for each habit w/ path '/habits/:id'.

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import HabitCard from './HabitCard'
+import { Link } from 'react-router-dom'
 
 const Habits = ({ habits }) => {
-    const habitCards = habits.map(h => <HabitCard habit={h} key={h.id} />)
-    return (
-        <div>
-            {habitCards}
-        </div>
-    );
+    const habitLinks = habits.length > 0 ?
+    habits.map(h => (<p key={h.id}><Link to={`/habits/${h.id}`}>{h.attributes.name}</Link></p>)) :
+    null
+    return habitLinks;
 };
 
 export default connect(state => ({habits: state.habits}))(Habits);
