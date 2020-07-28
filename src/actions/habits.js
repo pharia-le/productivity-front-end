4c63a4111cbfb51e2a834c3956a138830505d55d
pharia-le <pharia.le@gmail.com>
2020-07-28 10:11:47 -0700

    Update reducer & action for habits to include UPDATE_HABIT & add updateHabit fetch request.

--- src/actions/habits.js
+++ src/actions/habits.js
@@ -21,6 +21,13 @@ export const addHabit = habit => {
     }
 }
 
+export const updateHabitSuccess = habit => {
+    return {
+        type: "UPDATE_HABIT",
+        habit
+    }
+}
+
 // asynchronous action creators
 export const getHabits = () => {
     return dispatch => {
@@ -61,6 +68,37 @@ export const createHabit = (habitData, history) => {
                         alert(json.error)
                     } else {
                         dispatch(addHabit(json.data))
+                        dispatch(resetHabitForm())
+                        history.push(`/habits/${json.data.id}`)
+                    }
+                })
+        )
+    }
+}
+
+export const updateHabit = (habitData, history) => {
+    const sendableHabitData = {
+        habit: {
+            name: habitData.name,
+            id: habitData.habitId
+        }
+    }
+    return dispatch => {
+        return (
+            fetch(`http://localhost:3000/api/v1/habits/${habitData.habitId}`, {
+                credentials: "include",
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(sendableHabitData)
+                })
+                .then(resp => resp.json())
+                .then(json => {
+                    if (json.error) {
+                        alert(json.error)
+                    } else {
+                        dispatch(updateHabitSuccess(json.data))
                         dispatch(resetHabitForm())
                         history.push(`/habits/${json.data.id}`)
                     }
--- src/reducers/habits.js
+++ src/reducers/habits.js
@@ -6,6 +6,8 @@ export default (state = initialState, action) => {
             return action.habits
         case "ADD_HABIT":
             return state.concat(action.habit)
+        case "UPDATE_HABIT":
+            return state.map(habit => habit.id === action.habit.id ? action.habit : habit)
         case "CLEAR_HABITS":
             return initialState
         default:
