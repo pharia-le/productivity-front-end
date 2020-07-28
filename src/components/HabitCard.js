36352342c3b921a07b862b9ac6893238882eff50
pharia-le <pharia.le@gmail.com>
2020-07-28 00:12:42 -0700

    Update return to include Link to 'habit/:id/edit' & update imports.

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const HabitCard = ({ habit }) => {
     return (
         habit ?
         <div>
             <h3>{habit.attributes.name}</h3>
+            <Link 
+                to={`/habits/${habit.id}/edit`}
+                >Edit Habit 
+            </Link>
         </div>
         : null
     );
