aa9c2c4f15f5d3ff420ce83ccd1f1f44c8feac64
pharia-le <pharia.le@gmail.com>
2020-07-27 23:43:35 -0700

    Update return to conditionally render.

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const HabitCard = ({ habit }) => {
     return (
-        <p>{habit.attributes.name}</p>
+        habit ?
+        <div>
+            <h3>{habit.attributes.name}</h3>
+        </div>
+        : null
     );
 };
 
