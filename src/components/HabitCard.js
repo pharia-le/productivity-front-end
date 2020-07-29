5a621994ee840a675a370741253ca32a7745c56e
pharia-le <pharia.le@gmail.com>
2020-07-29 13:20:53 -0700

    Import Logs & add Logs component in return & pass props.

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Logs from './Logs'
 
 const HabitCard = ({ habit }) => {
     return (
-        habit ?
         <div>
-            <h3>{habit.attributes.name}</h3>
-            <Link 
-                to={`/habits/${habit.id}/edit`}
-                >Edit Habit 
-            </Link>
+            {habit ?
+            <div>
+                <h3>{habit.attributes.name}</h3>
+                <Link 
+                    to={`/habits/${habit.id}/edit`}
+                    >Edit Habit 
+                </Link>
+            </div>
+            : null}
+            <Logs logs={habit.attributes.logs} habitId={habit.id} />
         </div>
-        : null
     );
 };
 
