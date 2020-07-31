0b493de29c934d04c18d9cbc2f30911ae5f1a0c0
pharia-le <pharia.le@gmail.com>
2020-07-31 16:05:55 -0700

    Add dateToStr method.

--- src/components/LogCard.js
+++ src/components/LogCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const LogCard = ({ log, habitName }) => {
+    const dateToStr = (someDate) => {
+        return new Date(Date.parse(someDate)).toDateString()
+      }
     return (
         <div>
             <h3>{habitName}</h3>
-            <h2>Date: {log.date}</h2>
+            <h2>Date: {dateToStr(log.date)}</h2>
             <h2>Done: {log.done ? "YES" : "NO"}</h2>
             <Link 
                 to={`/habits/${log.habit_id}/logs/${log.id}/edit`}
--- src/components/Logs.js
+++ src/components/Logs.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Logs = ({ logs, habitId }) => {
+    
+    const dateToStr = (someDate) => {
+        return new Date(Date.parse(someDate)).toDateString()
+      }
     const logLinks = logs.length > 0 ?
-    logs.map(log => (<p key={log.id}><Link to={`/habits/${habitId}/logs/${log.id}`}>{log.date}</Link></p>)) :
+    logs.map(log => (<p key={log.id}><Link to={`/habits/${habitId}/logs/${log.id}`}>{dateToStr(log.date)}</Link></p>)) :
     null
     return logLinks;
 };
