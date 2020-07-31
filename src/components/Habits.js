15409570e0031816c002662507d83a989e18b16a
pharia-le <pharia.le@gmail.com>
2020-07-31 16:03:33 -0700

    Update to return IsLoggedToday & NotLoggedToday components.

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
+import IsLoggedToday from './IsLoggedToday'
+import NotLoggedToday from './NotLoggedToday'
 
 const Habits = ({ habits }) => {
+    
+    let isDate = date => {
+        const today = new Date()
+        date = new Date(Date.parse(someDate))
+        return (date.getDate() === date.getDate() && date.getMonth() === today.getMonth() && someDate.getFullYear() === today.getFullYear())
+      }
     const habitLinks = habits.length > 0 ?
-    habits.map(h => (<p key={h.id}><Link to={`/habits/${h.id}`}>{h.attributes.name}</Link></p>)) :
-    null
-    return habitLinks;
+    habits.map(h => (
+            <div key={h.id}>{(h.attributes.logs.find(l => isDate(l.created_at))) ? 
+                <IsLoggedToday habit={h} /> 
+                :    
+                <NotLoggedToday habit={h} />
+                }
+            </div>
+        )
+    ) 
+    : null
+    return (
+        <>
+            <h3>TODAY</h3>
+            <h5>{}</h5>
+            {habitLinks}
+        </>
+        )
 };
 
 export default connect(state => ({habits: state.habits}))(Habits);
