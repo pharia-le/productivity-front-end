3c96de8e3ab529ffc89b9569ec58c0e86426f358
pharia-le <pharia.le@gmail.com>
2020-07-31 16:01:20 -0700

    Update return to include Calendar component.

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import Logs from './Logs'
+import Calendar from './Calendar'
 
 const HabitCard = ({ habit }) => {
+
+    const habitCreatedDate = new Date(Date.parse(habit.attributes.created_at))
+    const calendarStartDate = new Date(habitCreatedDate.getFullYear(), habitCreatedDate.getMonth()-1, 1)
+
     return (
         <div>
             {habit ?
@@ -14,7 +18,11 @@ const HabitCard = ({ habit }) => {
                 </Link>
             </div>
             : null}
-            <Logs logs={habit.attributes.logs} habitId={habit.id} />
+                    
+            <Calendar logs={habit.attributes.logs} calendarStartDate={calendarStartDate}/>
+            
+            <h3>Data</h3>
+            <h4>Number of Logs: {habit.attributes.logs.length}</h4>
         </div>
     );
 };
