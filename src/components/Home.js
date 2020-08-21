ce64a00cdd59e3e5ec1952948e3ddc6df73a71ba
pharia-le <pharia.le@gmail.com>
2020-08-21 13:56:31 -0700

    Add heart img & app name.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className='centered'>
-            <h4>Welcome, please <Link to='/signup' >Sign Up</Link> or <Link to='/login' >Log In</Link></h4>
+            <h1>MOMENTUM</h1>
+            <br></br>
+            <img src={require('../heart.png')} alt="heart icon" height='370' width='370'/>
+            <br></br>
+            <br></br>
+            <h4><Link to='/signup' >sign up</Link> / <Link to='/login' >log in</Link></h4>
         </div>
     );
 };
