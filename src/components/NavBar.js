25a8f41056960531470447d744e7bc4e3f655a74
pharia-le <pharia.le@gmail.com>
2020-08-21 13:58:45 -0700

    Change buttons to a dropdown menu.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,28 +4,35 @@ import { logout } from '../actions/currentUser';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { LinkContainer } from 'react-router-bootstrap'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavBar = ({ logout }) => {
     return (
         <Navbar sticky="top" expand="lg" variant="dark" bg="dark">
             <Container id='navbar-container'>
-                <Navbar.Brand to="/">Productivity</Navbar.Brand>
+                <Navbar.Brand to="/">Momentum</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav className="mr-auto">
-                        <LinkContainer 
-                            to="/habits"
-                            ><button>Habits</button>
-                        </LinkContainer>
-                        <LinkContainer
-                            to="/habits/new"
-                            ><button>Create</button>
-                        </LinkContainer>
-                        <LinkContainer 
-                            to="/#"
-                            onClick={logout}
-                            ><button>Log Out</button>
-                        </LinkContainer>
+                        <NavDropdown title="Menu" id="basic-nav-dropdown">
+                            <LinkContainer 
+                                to="/habits/#"
+                                ><NavDropdown.Item>Home</NavDropdown.Item>
+                            </LinkContainer>
+                            <LinkContainer
+                                to="/habits/new"
+                                ><NavDropdown.Item>Create</NavDropdown.Item>
+                            </LinkContainer>
+                            <NavDropdown.Divider/>
+                            <LinkContainer 
+                                to="/#"
+                                onClick={logout}
+                                ><NavDropdown.Item>Log Out</NavDropdown.Item>
+                            </LinkContainer>
+                        </NavDropdown>
                     </Nav>
+                </Navbar.Collapse> 
             </Container>
         </Navbar>
     );
